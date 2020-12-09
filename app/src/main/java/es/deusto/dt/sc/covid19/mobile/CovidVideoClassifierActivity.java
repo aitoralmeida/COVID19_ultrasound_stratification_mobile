ca3436185766ee73e87839f9e3256dba6607fb33
UnaiBermejo <unaibermejofdez@opendeusto.es>
2020-12-09 14:32:47 +0100

    Added TF lite native interpreter and adapted code to remote model

--- app/src/main/java/es/deusto/dt/sc/covid19/mobile/CovidVideoClassifierActivity.java
+++ app/src/main/java/es/deusto/dt/sc/covid19/mobile/CovidVideoClassifierActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,40 +16,49 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.common.FirebaseMLException;
-import com.google.firebase.ml.custom.FirebaseCustomLocalModel;
-import com.google.firebase.ml.custom.FirebaseModelDataType;
-import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
-import com.google.firebase.ml.custom.FirebaseModelInputs;
-import com.google.firebase.ml.custom.FirebaseModelInterpreter;
-import com.google.firebase.ml.custom.FirebaseModelInterpreterOptions;
-import com.google.firebase.ml.custom.FirebaseModelOutputs;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseCustomRemoteModel;
+
+import org.tensorflow.lite.Interpreter;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 public class CovidVideoClassifierActivity extends AppCompatActivity {
 
-    private static final String TAG = "CovidVideoClassifier";
+    private static final String TAG = "CovidImageClassifier";
 
-    private static final String MODEL_NAME = "mobilenet_lstm_5_frames.tflite";
+    private static final String MODEL_NAME = "mobilenet_lstm_covid";
     private static final String LABELS_NAME = "labels.txt";
     private static final String SAMPLE_IMAGE = "06_N1_B0_P0_C0_M0_S0_F0044.png";
 
     private static final int IMAGE_WIDTH = 224;
     private static final int IMAGE_HEIGHT = 224;
+    private static final int CHANNELS = 3;
     private static final int FRAMES = 5;
 
-    private FirebaseCustomLocalModel mLocalModel;
-    private FirebaseModelInterpreter mInterpreter;
-    private FirebaseModelInputOutputOptions mInputOutputOptions;
-    private FirebaseModelInputs mInputs;
+    private static final int CLASSES = 2;
+
+    private static final int NUM_THREADS = 4;
+
+    private FirebaseCustomRemoteModel mRemoteModel;
+    private FirebaseModelDownloadConditions mModelDownloadConditions;
+    private Interpreter mInterpreter;
 
     private Bitmap mBitmap;
+
+    private ByteBuffer mInput;
+    private ByteBuffer mOutput;
 
     private ImageView mImageView;
     private Button mPredictButton;
@@ -63,11 +73,31 @@ public class CovidVideoClassifierActivity extends AppCompatActivity {
         mPredictButton = findViewById(R.id.predict);
         mTextView = findViewById(R.id.prediction);
 
-        initializeLocalModel();
-
-        initializeInterpreter();
-
-        initializeInputOutputOptions();
+        mRemoteModel = new FirebaseCustomRemoteModel.Builder(MODEL_NAME).build();
+        mModelDownloadConditions = new FirebaseModelDownloadConditions.Builder().requireWifi().build();
+        FirebaseModelManager.getInstance().download(mRemoteModel, mModelDownloadConditions)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void v) {
+                        // Download complete. Depending on your app, you could enable
+                        // the ML feature, or switch from the local model to the remote
+                        // model, etc.
+                        Log.d(TAG, "Downloaded model!");
+                    }
+                });
+        FirebaseModelManager.getInstance().getLatestModelFile(mRemoteModel)
+                .addOnCompleteListener(new OnCompleteListener<File>() {
+                    @Override
+                    public void onComplete(@NonNull Task<File> task) {
+                        File modelFile = task.getResult();
+                        if (modelFile != null) {
+                            Interpreter.Options options = new Interpreter.Options();
+                            options.setNumThreads(NUM_THREADS);
+                            mInterpreter = new Interpreter(modelFile, options);
+                            Log.d(TAG, "Constructed interpreter!");
+                        }
+                    }
+                });
 
         // LOAD SAMPLE IMAGE TO CLASSIFY -------------------------------------------------------
         mBitmap = getBitmapFromAsset(this, SAMPLE_IMAGE);
@@ -75,106 +105,63 @@ public class CovidVideoClassifierActivity extends AppCompatActivity {
         mImageView.setImageBitmap(mBitmap);
         // -------------------------------------------------------------------------------------
 
+        mInput = ByteBuffer.allocateDirect(IMAGE_WIDTH * IMAGE_HEIGHT * CHANNELS * FRAMES * 4).order(ByteOrder.nativeOrder());
+        for (int frame = 0; frame < FRAMES; frame++) {
+            for (int y = 0; y < IMAGE_WIDTH; y++) {
+                for (int x = 0; x < IMAGE_HEIGHT; x++) {
+                    int px = mBitmap.getPixel(x, y);
+                    // Get channel values from the pixel value.
+                    int r = Color.red(px);
+                    int g = Color.green(px);
+                    int b = Color.blue(px);
+                    // Normalize channel values to [-1.0, 1.0]. This requirement depends
+                    // on the model. For example, some models might require values to be
+                    // normalized to the range [0.0, 1.0] instead.
+                    float rf = (r - 127) / 255.0f;
+                    float gf = (g - 127) / 255.0f;
+                    float bf = (b - 127) / 255.0f;
+                    mInput.putFloat(rf);
+                    mInput.putFloat(gf);
+                    mInput.putFloat(bf);
+                }
+            }
+        }
+
+        int bufferSize = CLASSES * Float.SIZE / Byte.SIZE;
+        mOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder());
+
         mPredictButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                int num = 0;
-                float[][][][][] input = new float[1][FRAMES][IMAGE_WIDTH][IMAGE_HEIGHT][3];
-                for (int frame = 0; frame < FRAMES; frame++) {
-                    for (int x = 0; x < IMAGE_WIDTH; x++) {
-                        for (int y = 0; y < IMAGE_HEIGHT; y++) {
-                            int pixel = mBitmap.getPixel(x, y);
-                            //TODO Check if this mapping is appropriate
-                            input[num][frame][x][y][0] = (Color.red(pixel) - 127) / 128.0f;
-                            input[num][frame][x][y][1] = (Color.green(pixel) - 127) / 128.0f;
-                            input[num][frame][x][y][2] = (Color.blue(pixel) - 127) / 128.0f;
-                        }
-                    }
-                }
-                addInputToModel(input);
                 runInterpreter();
             }
         });
+
     }
 
     private void runInterpreter() {
         Log.d(TAG, "Running interpreter");
-        mInterpreter.run(mInputs, mInputOutputOptions)
-                .addOnSuccessListener(
-                        new OnSuccessListener<FirebaseModelOutputs>() {
-                            @Override
-                            public void onSuccess(FirebaseModelOutputs result) {
-                                Log.d(TAG, "Running interpreter succeeded");
-                                try {
-                                    float[][] output = result.getOutput(0);
-                                    float[] probabilities = output[0];
-                                    BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(LABELS_NAME)));
-                                    String formattedResult = "";
-                                    Log.d(TAG, "Results for " + SAMPLE_IMAGE);
-                                    for (int i = 0; i < probabilities.length; i++) {
-                                        String label = reader.readLine();
-                                        String resultForLabel = String.format("%s (%d): %1.4f", label, i, probabilities[i]);
-                                        Log.d(TAG, resultForLabel);
-                                        formattedResult = formattedResult + resultForLabel + "\n";
-                                    }
-                                    mTextView.setText(formattedResult);
-                                } catch (IOException e) {
-                                    Log.d(TAG, "Could not read labels file with name " + LABELS_NAME + " from assets");
-                                    e.printStackTrace();
-                                }
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d(TAG, "Running interpreter FAILED");
-                                e.printStackTrace();
-                            }
-                        });
-    }
-
-    private void addInputToModel(float[][][][][] input) {
-        Log.d(TAG, "Building and adding input to model");
+        final long startTime = SystemClock.uptimeMillis();
+        mInterpreter.run(mInput, mOutput);
+        final long inferenceTime = SystemClock.uptimeMillis() - startTime;
+        Log.d(TAG, "Running interpreter succeeded");
+        Log.d(TAG, "Inference time: " + inferenceTime);
+        mOutput.rewind();
+        FloatBuffer probabilities = mOutput.asFloatBuffer();
         try {
-            mInputs = new FirebaseModelInputs.Builder()
-                    .add(input)
-                    .build();
-        } catch (FirebaseMLException e) {
-            Log.d(TAG, "Building and adding input to model FAILED");
-            e.printStackTrace();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open(LABELS_NAME)));
+            String formattedResult = "";
+            Log.d(TAG, "Results for " + SAMPLE_IMAGE);
+            for (int i = 0; i < probabilities.capacity(); i++) {
+                String label = reader.readLine();
+                String resultForLabel = String.format("%s (%d): %1.4f", label, i, probabilities.get(i));
+                Log.d(TAG, resultForLabel);
+                formattedResult = formattedResult + resultForLabel + "\n";
+            }
+            mTextView.setText(formattedResult);
+        } catch (IOException e) {
+            // File not found?
         }
-    }
-
-    private void initializeInputOutputOptions() {
-        Log.d(TAG, "Building input and output options");
-        try {
-            mInputOutputOptions = new FirebaseModelInputOutputOptions.Builder()
-                            .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, FRAMES, IMAGE_WIDTH, IMAGE_HEIGHT, 3})
-                            .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 2})
-                            .build();
-        } catch (FirebaseMLException e) {
-            Log.d(TAG, "Building input and output options FAILED");
-            e.printStackTrace();
-        }
-    }
-
-    private void initializeInterpreter() {
-        Log.d(TAG, "Building interpreter");
-        try {
-            FirebaseModelInterpreterOptions options =
-                    new FirebaseModelInterpreterOptions.Builder(mLocalModel).build();
-            mInterpreter = FirebaseModelInterpreter.getInstance(options);
-        } catch (FirebaseMLException e) {
-            Log.d(TAG, "Building interpreter FAILED");
-            e.printStackTrace();
-        }
-    }
-
-    private void initializeLocalModel() {
-        Log.d(TAG, "Getting and building local model " + MODEL_NAME + " from assets");
-        mLocalModel = new FirebaseCustomLocalModel.Builder()
-                .setAssetFilePath(MODEL_NAME)
-                .build();
     }
 
     private static Bitmap getBitmapFromAsset(Context context, String filePath) {
